3ee899e18642e89c10ceece342608ccc5c22b909
Emilio5499 <1448592@alu.murciaeduca.es>
2025-05-25 14:39:33 +0200

    Comando 2 comentarios

---
+++ app/Console/Commands/LimpiarComentariosInactivos.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Comentario;
+use Carbon\Carbon;
+use Illuminate\Console\Command;
+
+class LimpiarComentariosInactivos extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'comentarios:limpiar-inactivos
+                            {--dias=60 : Se borran comentarios viejos}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Borra comentarios viejos';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $limite = Carbon::now()->subDays($this->option('dias'));
+
+        $comentarios = Comentario::where(function ($query) {
+            $query->whereNull('content')
+                ->orWhere('content', '');
+        })
+            ->where('created_at', '<=', $limite)
+            ->get();
+
+        $total = $comentarios->count();
+
+        if ($total === 0) {
+            $this->info('No hay comentarios que eliminar.');
+            return Command::SUCCESS;
+        }
+
+        Comentario::whereIn('id', $comentarios->pluck('id'))->delete();
+
+        $this->info("$total comentarios borrados.");
+
+        return Command::SUCCESS;
+    }
+}

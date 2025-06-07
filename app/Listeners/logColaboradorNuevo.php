83eefd76fc7760db24df18f3e5fd277a3920a5bd
Emilio5499 <1448592@alu.murciaeduca.es>
2025-06-07 11:17:43 +0200

    mails

--- app/Listeners/logColaboradorNuevo.php
+++ app/Listeners/logColaboradorNuevo.php
@@ -5,6 +5,7 @@ namespace App\Listeners;
 use App\Events\ColaboradorNuevo;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Log;
 
 class logColaboradorNuevo
 {

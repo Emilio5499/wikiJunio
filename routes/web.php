21e6476be3b97561ef0cd1527a2724e873e59024
Emilio5499 <1448592@alu.murciaeduca.es>
2026-01-19 09:34:53 +0100

    Ordenando web

--- routes/web.php
+++ routes/web.php
@@ -16,46 +16,62 @@ Route::middleware([SetLocale::class])->group(function () {
         return redirect()->back();
     })->name('lang.switch');
 
-    Route::middleware(['auth'])->group(function () {
-        Route::get('/articles/{article}/edit', [PublicArticleController::class, 'edit'])->name('articles.edit');
-        Route::put('/articles/{article}', [PublicArticleController::class, 'update'])->name('articles.update');
-    });
+    Route::get('/', [PublicArticleController::class, 'index'])
+        ->name('public.articles.index');
+
+    Route::get('/articles/{article}', [PublicArticleController::class, 'show'])
+        ->name('public.articles.show');
 
     Route::get('/articles/create', function () {
         return view('articles.create');
     })->name('articles.create');
 
-    Route::get('/articles/{article}/download-pdf', [ArticlePdfController::class, 'download'])
-        ->middleware('auth')
-        ->name('articles.downloadPdf');
+    Route::get('/wiki', [PublicArticleController::class, 'index'])
+        ->name('wiki.index');
 
-    Route::get('/articles/downloadAll', [ArticlePdfController::class, 'downloadAll'])
-        ->middleware('auth')
-        ->name('articles.downloadAll');
+    Route::get('/wiki/{article}', [PublicArticleController::class, 'show'])
+        ->name('wiki.show');
 
-    Route::get('/', [PublicArticleController::class, 'index'])->name('public.articles.index');
-    Route::get('/articles/{article}', [PublicArticleController::class, 'show'])->name('public.articles.show');
+    Route::middleware(['auth'])->group(function () {
 
-    Route::get('/wiki', [PublicArticleController::class, 'index'])->name('wiki.index');
-    Route::get('/wiki/{article}', [PublicArticleController::class, 'show'])->name('wiki.show');
+        Route::get('/articles/{article}/edit', [PublicArticleController::class, 'edit'])
+            ->name('articles.edit');
 
-    Route::middleware(['auth', 'verified'])->group(function () {
-        Route::get('/dashboard', fn () => view('dashboard'))->name('dashboard');
+        Route::put('/articles/{article}', [PublicArticleController::class, 'update'])
+            ->name('articles.update');
 
-        Route::middleware(['auth'])->group(function () {
-            Route::get('/profile', [ProfileController::class, 'edit'])->name('profile');
-            Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-            Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-            Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-            Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-        });
+        Route::get('/articles/{article}/download-pdf', [ArticlePdfController::class, 'download'])
+            ->name('articles.downloadPdf');
 
-        Route::post('/articles', [ArticleApiController::class, 'store'])->middleware('auth:sanctum');
+        Route::get('/articles/downloadAll', [ArticlePdfController::class, 'downloadAll'])
+            ->name('articles.downloadAll');
     });
 
-    Route::middleware(['auth', 'permission:manage categories'])->get('/admin/categories', fn () => view('admin.categories'))->name('admin.categories');
-    Route::middleware(['auth', 'permission:manage tags'])->get('/admin/tags', fn () => view('admin.tags'))->name('admin.tags');
+    Route::middleware(['auth', 'verified'])->group(function () {
+
+        Route::get('/dashboard', fn () => view('dashboard'))
+            ->name('dashboard');
+
+        Route::get('/profile', [ProfileController::class, 'edit'])
+            ->name('profile.edit');
+
+        Route::patch('/profile', [ProfileController::class, 'update'])
+            ->name('profile.update');
+
+        Route::delete('/profile', [ProfileController::class, 'destroy'])
+            ->name('profile.destroy');
+
+        Route::post('/articles', [ArticleApiController::class, 'store'])
+            ->middleware('auth:sanctum');
+    });
+
+    Route::middleware(['auth', 'permission:manage categories'])
+        ->get('/admin/categories', fn () => view('admin.categories'))
+        ->name('admin.categories');
+
+    Route::middleware(['auth', 'permission:manage tags'])
+        ->get('/admin/tags', fn () => view('admin.tags'))
+        ->name('admin.tags');
 });
 
-require __DIR__.'/auth.php';
-
+require __DIR__ . '/auth.php';

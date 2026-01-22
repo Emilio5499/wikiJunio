8b88f2c6696ed4d757e91092b0471ff5d7e49888
Emilio5499 <1448592@alu.murciaeduca.es>
2026-01-22 11:42:24 +0100

    rutas arregladas

--- routes/web.php
+++ routes/web.php
@@ -19,50 +19,48 @@ Route::middleware([SetLocale::class])->group(function () {
     Route::get('/', [PublicArticleController::class, 'index'])
         ->name('public.articles.index');
 
-    Route::get('/articles/{article}', [PublicArticleController::class, 'show'])
-        ->name('public.articles.show');
-
-    Route::get('/articles/create', function () {
-        return view('articles.create');
-    })->name('articles.create');
-
     Route::get('/wiki', [PublicArticleController::class, 'index'])
         ->name('wiki.index');
 
     Route::get('/wiki/{article}', [PublicArticleController::class, 'show'])
         ->name('wiki.show');
 
+    Route::get('/articles/create', function () {
+        return view('articles.create');
+    })->name('articles.create');
+
     Route::middleware(['auth'])->group(function () {
+
+        // ⚠️ MUY IMPORTANTE: rutas fijas primero
+        Route::get('/articles/downloadAll', [ArticlePdfController::class, 'downloadAll'])
+            ->name('articles.downloadAll');
+
+        Route::get('/articles/{article}/download-pdf', [ArticlePdfController::class, 'download'])
+            ->name('articles.downloadPdf');
 
         Route::get('/articles/{article}/edit', [PublicArticleController::class, 'edit'])
             ->name('articles.edit');
 
         Route::put('/articles/{article}', [PublicArticleController::class, 'update'])
             ->name('articles.update');
-
-        Route::get('/articles/{article}/download-pdf', [ArticlePdfController::class, 'download'])
-            ->name('articles.downloadPdf');
-
-        Route::get('/articles/downloadAll', [ArticlePdfController::class, 'downloadAll'])
-            ->name('articles.downloadAll');
     });
+
+    Route::get('/articles/{article}', [PublicArticleController::class, 'show'])
+        ->name('public.articles.show');
 
     Route::middleware(['auth', 'verified'])->group(function () {
 
         Route::get('/dashboard', fn () => view('dashboard'))
             ->name('dashboard');
 
-        Route::middleware(['auth'])->group(function () {
+        Route::get('/profile', [ProfileController::class, 'edit'])
+            ->name('profile.edit');
 
-            Route::get('/profile', [ProfileController::class, 'edit'])
-                ->name('profile.edit');
+        Route::patch('/profile', [ProfileController::class, 'update'])
+            ->name('profile.update');
 
-            Route::patch('/profile', [ProfileController::class, 'update'])
-                ->name('profile.update');
-
-            Route::delete('/profile', [ProfileController::class, 'destroy'])
-                ->name('profile.destroy');
-        });
+        Route::delete('/profile', [ProfileController::class, 'destroy'])
+            ->name('profile.destroy');
 
         Route::post('/articles', [ArticleApiController::class, 'store'])
             ->middleware('auth:sanctum');

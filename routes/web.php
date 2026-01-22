b344b8bc2c782855d7cc8f326b617112a7acf60b
Emilio5499 <1448592@alu.murciaeduca.es>
2026-01-22 11:20:20 +0100

    arreglando rutas otra vez

--- routes/web.php
+++ routes/web.php
@@ -52,14 +52,17 @@ Route::middleware([SetLocale::class])->group(function () {
         Route::get('/dashboard', fn () => view('dashboard'))
             ->name('dashboard');
 
-        Route::get('/profile', [ProfileController::class, 'edit'])
-            ->name('profile.edit');
+        Route::middleware(['auth'])->group(function () {
 
-        Route::patch('/profile', [ProfileController::class, 'update'])
-            ->name('profile.update');
+            Route::get('/profile', [ProfileController::class, 'edit'])
+                ->name('profile.edit');
 
-        Route::delete('/profile', [ProfileController::class, 'destroy'])
-            ->name('profile.destroy');
+            Route::patch('/profile', [ProfileController::class, 'update'])
+                ->name('profile.update');
+
+            Route::delete('/profile', [ProfileController::class, 'destroy'])
+                ->name('profile.destroy');
+        });
 
         Route::post('/articles', [ArticleApiController::class, 'store'])
             ->middleware('auth:sanctum');
@@ -74,4 +77,4 @@ Route::middleware([SetLocale::class])->group(function () {
         ->name('admin.tags');
 });
 
-require __DIR__ . '/auth.php';
+require __DIR__.'/auth.php';

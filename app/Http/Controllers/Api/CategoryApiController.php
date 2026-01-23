a1c8ae5c45ca67995d926bafc97e3ebb31fabd04
Emilio5499 <1448592@alu.murciaeduca.es>
2026-01-23 11:51:25 +0100

    categoryApi show

--- app/Http/Controllers/Api/CategoryApiController.php
+++ app/Http/Controllers/Api/CategoryApiController.php
@@ -13,8 +13,14 @@ class CategoryApiController extends Controller
         return Category::withCount('articles')->get();
     }
 
-    public function show(){
-
+    public function show($id)
+    {
+        $category = Category::whereHas('articles', function ($query) {
+            $query->where('user_id', auth()->id());
+        })
+            ->with('articles')
+            ->findOrFail($id);
+        return response()->json($category);
     }
 
     public function update(Request $request, $id){
